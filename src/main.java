f91f3118f7e88c528fb6ae9467e564369cfbd583
Maria Bistrova <luma000.27@gmail.com>
2022-02-10 23:01:04 +0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class main {
+    public static void main(String[] args) {
+        int balance = 300;
+        int replenishBalance = 1500;
+
+        int bonus = replenishBalance >= 1000 ? replenishBalance / 100 : 0;
+
+        int overallBalance = replenishBalance + balance + bonus;
+        System.out.println("Общий баланс:" + overallBalance);
+        System.out.println("бонус:" + bonus);
+    }
+}
